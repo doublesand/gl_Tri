0e067bbd9215cd7370a1ff1ebb7438bfc4c23278
doublesand <doublesand@163.com>
2018-11-21 18:39:30 +0800

    成功创建子窗口

--- gl_Tri/main.cpp
+++ gl_Tri/main.cpp
@@ -15,28 +15,24 @@ const vec3 PURPLE(0.8, 0.0, 0.8);
 //顶点个数和颜色个数
 const int NUM_POINTS = 4;
 const int NUM_COLORS = 4;
+vec2 vertices[NUM_POINTS];
+vec3 colors[NUM_COLORS] = {
+	PURPLE, BLUE, RED, ORANGE
+};
 
 //主窗口
 GLuint win;
 const int width = 512;
 const int height = 512;
-
 GLuint menu;
 double angle = 0;
 
-void changeCenter(vec2 vertices[], vec2 center) {
-	for (int i = 0; i < NUM_POINTS; i++) {
-		vertices[i] += center;
-	}
-}
+//子窗口
+GLuint subWin;
+vec2 subVertices[3] = {vec2(-0.75,-0.75), vec2(0,0.75) ,vec2(0.75,-0.75)};
+vec3 subColors[3] = {BLUE, RED, GREEN};
 
-void changeShape(vec2 vertices[], double rate) {
-	for (int i = 0; i < NUM_POINTS; i++) {
-		vertices[i] = vertices[i] * rate;
-	}
-}
-
-void setSqure(vec2 vertices[]) {
+void setSqure(vec2 vertices[]) {     //改变角度
 	for (int i = 0; i < NUM_POINTS; i++) {
 		double currentAngle = (M_PI / 4 + (M_PI / 2 * i)) + angle;
 		vertices[i] = vec2(sin(currentAngle), cos(currentAngle)) * 0.5;
@@ -45,12 +41,7 @@ void setSqure(vec2 vertices[]) {
 
 void init() {
 	//一个窗口的宽度是2，左右分别是-1和1，所以超过1的都不能看见
-	vec2 vertices[NUM_POINTS];
 	setSqure(vertices);
-	vec3 colors[NUM_COLORS] = {
-		PURPLE, BLUE, RED, ORANGE
-	};
-
 
 	//创建顶点数组对象
 	GLuint vao;
@@ -80,7 +71,7 @@ void init() {
 	GLuint clocation = glGetAttribLocation(program, "vColor");
 	glEnableVertexAttribArray(clocation);
 	glVertexAttribPointer(clocation, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(vertices)));
-	// 白色背景
+	// 黑色背景
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 }
 
@@ -89,7 +80,29 @@ void idle() {  //空闲回调函数
 	if (angle > M_PI*2) {
 		angle - M_PI * 2;
 	}
-	glutPostRedisplay();
+	glutPostWindowRedisplay(win);
+}
+
+void mouse(int button, int state, int x, int y) {
+	if (button == GLUT_MIDDLE_BUTTON && state == GLUT_DOWN) {
+		glutIdleFunc(idle);
+	}
+	else if(button == GLUT_MIDDLE_BUTTON && state == GLUT_UP){
+		glutIdleFunc(NULL);
+	}
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case '\033':
+		exit(EXIT_SUCCESS);
+		break;
+	case 'b':
+		colors[0] = BLUE;
+		break;
+	}
+	glutPostWindowRedisplay(win);
 }
 
 void menuEvents(int id) {
@@ -105,8 +118,7 @@ void menuEvents(int id) {
 		glutIdleFunc(NULL);
 		break;
 	}
-	
-	glutPostRedisplay();  //做了修改后就进行重新绘制窗口
+	glutPostWindowRedisplay(win);
 }
 
 void setupMenu() {
@@ -129,6 +141,64 @@ void display(void)
 	//glFlush();
 }
 
+void subKeyboard(unsigned char key, int x, int y) {
+	switch (key)
+	{
+	case '\033':
+		exit(EXIT_SUCCESS);
+		break;
+	case 'b':
+		subColors[0] = BLUE;
+		break;
+	case 'o':
+		subColors[0] = ORANGE;
+		break;
+	}
+	glutPostWindowRedisplay(subWin);
+}
+
+void subInit() {
+	//创建数组对象并绑定
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	//创建并初始化顶点缓存对象缓存
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(subVertices) + sizeof(subColors), NULL, GL_STATIC_DRAW);
+
+	// 分别读取数据
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(subVertices), subVertices);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(subVertices), sizeof(subColors), subColors);
+
+	// 读取着色器并复用
+	GLuint program = InitShader("vshader.glsl", "fshader.glsl");
+	glUseProgram(program);
+
+	// 从顶点着色器中初始化顶点的位置
+	GLuint pLocation = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(pLocation);
+	glVertexAttribPointer(pLocation, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+	// 从片元着色器中初始化顶点的颜色
+	GLuint cLocation = glGetAttribLocation(program, "vColor");
+	glEnableVertexAttribArray(cLocation);
+	glVertexAttribPointer(cLocation, 3, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(subVertices)));
+
+	// 设置子窗口背景颜色为白色
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+
+
+void subDisplay() {
+	subInit();    // 重绘时写入新的颜色数据
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
+	glutSwapBuffers();
+}
+
 int main(int argc, char **argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);  //通过使用双缓存，使得画面更加柔顺
@@ -144,9 +214,16 @@ int main(int argc, char **argv) {
 
 	init();
 	setupMenu();
+	glutMouseFunc(mouse);
 	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+
+	//添加子窗口,以下语义都是给子窗口的
+	subWin = glutCreateSubWindow(win, 0, 0, width / 4, height / 4);  
+	subInit();
+	subDisplay();
+	glutKeyboardFunc(subKeyboard);
 
 	glutMainLoop();
-
 	return 0;
 }
